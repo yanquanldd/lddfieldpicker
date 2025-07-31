b293b9a89f5be572e545091585857ed58bcac917
Aeron Chen <yanquan@ldd.ca>
2025-07-31 14:33:00 -0400

    update gitignore, webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const devCerts = require("office-addin-dev-certs");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
+const path = require("path");
 
 const urlDev = "https://localhost:3000/";
 const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
@@ -24,6 +25,8 @@ module.exports = async (env, options) => {
       commands: "./src/commands/commands.ts",
     },
     output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].[contenthash].js",
       clean: true,
     },
     resolve: {
